a0f6123de973c07891abbb0e3f62b10f0d828259
Gabriel Miranda <gabrielmirandabarros98@gmail.com>
2023-04-04 23:25:32 -0300

    Produto-Categoria-Tipo

---
+++ src/main/java/com/primeirotrampodev/produtoCategoriaTipo/ProdutoCategoriaTipoApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.produtoCategoriaTipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoCategoriaTipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoCategoriaTipoApplication.class, args);
+	}
+
+}
